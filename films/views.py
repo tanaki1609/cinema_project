96e2feb7e520df34c6ed415c1d1097a7fe12c76d
tanaki <tanaki.93@gmail.com>
2026-03-03 18:54:32 +0600

    lesson 3

--- films/views.py
+++ films/views.py
@@ -5,29 +5,67 @@ from .models import Film
 from .serializers import FilmListSerializer, FilmDetailSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def film_detail_api_view(request, id):
     try:
         film = Film.objects.get(id=id)
     except Film.DoesNotExist:
         return Response(data={'error': 'film does not exist!'},
                         status=status.HTTP_404_NOT_FOUND)
-    data = FilmDetailSerializer(film, many=False).data
-    return Response(data=data)
+    if request.method == 'GET':
+        data = FilmDetailSerializer(film, many=False).data
+        return Response(data=data)
+    elif request.method == 'DELETE':
+        film.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        film.title = request.data.get('title')
+        film.text = request.data.get('text')
+        film.release_year = request.data.get('release_year')
+        film.rating = request.data.get('rating')
+        film.is_hit = request.data.get('is_hit')
+        film.director_id = request.data.get('director_id')
+        film.genres.set(request.data.get('genres'))
+        film.save()
+        return Response(status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def film_list_api_view(request):
-    # step 1: Collect films from DB (QuerySet)
-    films = Film.objects.select_related('director').prefetch_related(
-        'genres',
-        'reviews').all()
+    if request.method == 'GET':
+        # step 1: Collect films from DB (QuerySet)
+        films = Film.objects.select_related('director').prefetch_related(
+            'genres',
+            'reviews').all()
 
-    # step 2: Reformat (Serialize) films
-    data = FilmListSerializer(films, many=True).data
+        # step 2: Reformat (Serialize) films
+        data = FilmListSerializer(films, many=True).data
 
-    # step 3: Return Response
-    return Response(
-        data=data,
-        status=status.HTTP_200_OK
-    )
+        # step 3: Return Response
+        return Response(
+            data=data,
+            status=status.HTTP_200_OK
+        )
+    elif request.method == 'POST':
+        # step 1: Receive data from RequestBody
+        title = request.data.get('title')
+        text = request.data.get('text')
+        release_year = request.data.get('release_year')
+        rating = request.data.get('rating')
+        is_hit = request.data.get('is_hit')
+        director_id = request.data.get('director_id')
+        genres = request.data.get('genres')
+
+        # step 2: Create film
+        film = Film.objects.create(
+            title=title,
+            text=text,
+            release_year=release_year,
+            rating=rating,
+            is_hit=is_hit,
+            director_id=director_id,
+        )
+        film.genres.set(genres)
+        film.save()
+        # step 3: Return Response
+        return Response(status=status.HTTP_201_CREATED)
